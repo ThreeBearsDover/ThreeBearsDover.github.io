68192089232d6c80c137920d392381b3d5a45444
Anton Bredl <antonbr13@gmail.com>
2016-11-15 09:50:12 -0800

    setting up repo

---
+++ js/app.js
@@ -0,0 +1,48 @@
+angular.module('threebears', ['ui.router'])
+
+
+
+
+
+
+
+.config( function($stateProvider, $urlRouterProvider) {
+
+   $stateProvider
+
+      .state('home', {
+         url           : '/',
+         templateUrl   : 'js/home/homeView.html',
+         controller    : 'homeCtrl'
+      })
+
+      .state('bungalow', {
+         url           : '/bungalow',
+         templateUrl   : 'js/bungalow/bungalowView.html',
+         controller    : 'bungalowCtrl'
+      })
+
+      .state('cottage', {
+         url           : '/cottage',
+         templateUrl   : 'js/cottage/cottageView.html',
+         controller    : 'cottageCtrl'
+      })
+
+      .state('resort', {
+         url           : '/resort',
+         templateUrl   : 'js/resort/resortView.html',
+         controller    : 'resortCtrl'
+      })
+
+      .state('sandpoint', {
+         url           : '/sandpoint',
+         templateUrl   : 'js/sandpoint/sandpointView.html',
+         controller    : 'sandpointCtrl'
+      });
+
+
+
+   $urlRouterProvider
+      .otherwise('/');
+
+});
